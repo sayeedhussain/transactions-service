eec870cd2d6bc95bd9f9fa639e5ccf5b5fb5854f
Sayeed Munawar Hussain <sayeedhussain19@gmail.com>
2024-11-01 10:29:09 +0530

    Add dummy private / public keys to generate jwt token for oauth tests

---
+++ src/test/java/com/example/integrationtests/security/JwtUtil.java
@@ -0,0 +1,102 @@
+package com.example.integrationtests.security;
+
+import com.nimbusds.jose.JWSAlgorithm;
+import com.nimbusds.jose.jwk.*;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+import java.io.InputStream;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.security.KeyFactory;
+import java.security.PrivateKey;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.PKCS8EncodedKeySpec;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.Date;
+import java.util.Map;
+import java.util.UUID;
+
+public class JwtUtil {
+
+    public static String generateJwt(
+            String issuer,
+            Map<String, Object> claims
+    ) throws Exception {
+        // Load private key
+        PrivateKey privateKey = loadPrivateKey();
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        return Jwts.builder()
+                .setIssuer(issuer)
+                .addClaims(claims)
+                .setIssuedAt(now)
+                .setExpiration(new Date(nowMillis + 60000)) // 1 min expiration
+                .signWith(privateKey, SignatureAlgorithm.RS256)
+                .compact();
+    }
+
+    private static PrivateKey loadPrivateKey() throws Exception {
+        byte[] keyBytes;
+        try (InputStream inputStream = JwtUtil.class.getClassLoader().getResourceAsStream("private.pem")) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("File not found: private.pem");
+            }
+
+            keyBytes = inputStream.readAllBytes();
+        }
+        String privateKeyPEM = new String(keyBytes)
+                .replace("-----BEGIN PRIVATE KEY-----", "")
+                .replace("-----END PRIVATE KEY-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] decoded = Base64.getDecoder().decode(privateKeyPEM);
+        PKCS8EncodedKeySpec keySpec = new PKCS8EncodedKeySpec(decoded);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePrivate(keySpec);
+    }
+
+    public static String generateJwksFromPublicKey() throws Exception {
+        // Load public key
+        PublicKey publicKey = loadPublicKey();
+
+        // Convert the public key to an RSA JWK
+        RSAKey rsaJwk = new RSAKey.Builder((RSAPublicKey) publicKey)
+                .keyID(UUID.randomUUID().toString()) // Set a unique Key ID
+                .keyUse(KeyUse.SIGNATURE) // Specify use as signature
+                .algorithm(JWSAlgorithm.RS256) // Specify the algorithm
+                .build();
+
+        // Create a JWK Set with the RSA Key
+        JWKSet jwkSet = new JWKSet(rsaJwk);
+
+        // Output JWKS JSON
+        return jwkSet.toJSONObject().toString();
+    }
+
+    private static PublicKey loadPublicKey() throws Exception {
+        byte[] keyBytes;
+        try (InputStream inputStream = JwtUtil.class.getClassLoader().getResourceAsStream("public.pem")) {
+            if (inputStream == null) {
+                throw new IllegalArgumentException("File not found: public.pem");
+            }
+
+            keyBytes = inputStream.readAllBytes();
+        }
+
+        String publicKeyPEM = new String(keyBytes)
+                .replace("-----BEGIN PUBLIC KEY-----", "")
+                .replace("-----END PUBLIC KEY-----", "")
+                .replaceAll("\\s", "");
+
+        byte[] decoded = Base64.getDecoder().decode(publicKeyPEM);
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(decoded);
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        return keyFactory.generatePublic(keySpec);
+    }
+
+}
--- src/test/java/com/example/integrationtests/security/OAuthIntegrationTest.java
+++ src/test/java/com/example/integrationtests/security/OAuthIntegrationTest.java
@@ -38,11 +38,15 @@ public class OAuthIntegrationTest {
   @MockBean
   OrderService orderService;
 
+  private static String jwks;
+
   @BeforeAll
-  public static void setUp() {
+  public static void setUp() throws Exception {
     wireMockServer = new WireMockServer(8083);
     wireMockServer.start();
     configureFor("localhost", 8083);
+
+    jwks = JwtUtil.generateJwksFromPublicKey();
   }
 
   @AfterAll
@@ -54,7 +58,7 @@ public class OAuthIntegrationTest {
   public void shouldReturn401WhenNoTokenIsProvided() {
 
     // Given
-    stubForJWKS();
+    stubForJWKS(jwks);
 
     String url = url("/api/orders");
     HttpEntity<Map<String, Object>> requestEntity = createOrderRequestEntity(new HttpHeaders());
@@ -74,13 +78,13 @@ public class OAuthIntegrationTest {
   }
 
   @Test
-  public void shouldReturn403WhenScopeIsInvalid() {
+  public void shouldReturn403WhenScopeIsInvalid() throws Exception {
 
     // Given
-    stubForJWKS();
+    stubForJWKS(jwks);
 
-    String invalidToken =
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgzIiwiaWF0IjoxNzMwMzkzNDEzLCJleHAiOjE3MzA0Mjk0MTN9.GuDXssMfcbzsD45GFsP6DeXOj2PCbVRDl2mv5pNXI18MPvQmnd5BEMdebwnI2RpLfgfjQxOzFpdW77XHaQsf34d1FNWNVS7rcnsp3SQMqNzFhLuSc9nNa26ne2CMoqt7QH3By3MIH4X_1Clnxmsrr4K1aCNuHaBSmnq9en559BNKoHYaajI_27sWYaMs_IbzoY_ynD7EY2QaKRWHhE6Ipquphsdu5AjQhnZt6QmPqYCq5VuLFWpkw-OP5HFZOfmdReRjDzetA3J-gReHzmSTQcMX3IzjT3v2RqJ20MfYZ5-ep2GliMn3X1nml_YL1Y87kZkEgbPrOAEhRH62HdlR1w";
+    String invalidToken = JwtUtil.generateJwt("http://localhost:8083", Map.of("scope", "dummy"));
+
     HttpHeaders headers = new HttpHeaders();
     headers.setBearerAuth(invalidToken);
     String url = url("/api/orders");
@@ -101,13 +105,13 @@ public class OAuthIntegrationTest {
   }
 
   @Test
-  public void shouldReturn200WhenAccessTokenIsInvalid() {
+  public void shouldReturn200WhenAccessTokenIsInvalid() throws Exception {
 
     // Given
-    stubForJWKS();
+    stubForJWKS(jwks);
 
-    String validToken =
-        "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaXNzIjoiaHR0cDovL2xvY2FsaG9zdDo4MDgzIiwic2NvcGUiOiJvcmRlcjp3cml0ZSIsImlhdCI6MTczMDM5MzIxMywiZXhwIjoxNzMwNDI5MjEzfQ.B6bXWSbEF0C-CynitprR9P6NXkX23rD2683V6kanIvhkdqPEY57NzmhLf0TbrnDhOWVFz045HsJb56oYV2Cf1WQH6VXtV6ZGStA1jfQd30RmCPqrxFWcbC1cjbIZbLxCUvfVK-ND1vnNWg_rwLm0vACPEFQmAyGN8ItLVSTaq10iIHQvPUb51hosPQq7FqLjllxihQhQ7hT9V2sQy-sEgbWg_QeYC9-9-azw8hdzkQ1ZnGq9Xnml04K6ow_qd4h75aOKlyThtPoXDuFN7YorrhFHQMJhv3pWHir1h4sioKr71KnYC6THZUaiIWacmn3dqZNcJHPgKE5PsSBWOCJ4nQ";
+    String validToken = JwtUtil.generateJwt("http://localhost:8083", Map.of("scope", "order:write"));
+
     HttpHeaders headers = new HttpHeaders();
     headers.setBearerAuth(validToken);
     String url = url("/api/orders");
@@ -128,13 +132,12 @@ public class OAuthIntegrationTest {
     return "http://localhost:" + port + endpoint;
   }
 
-  private void stubForJWKS() {
+  private void stubForJWKS(String jwks) {
     stubFor(get("/.well-known/openid-configuration")
             .willReturn(okJson("{\"issuer\": \"http://localhost:8083\",\"jwks_uri\": \"http://localhost:8083/.well-known/jwks.json\"}")));
 
     stubFor(get("/.well-known/jwks.json")
-            .willReturn(okJson(
-                    "{\"keys\":[{\"e\":\"AQAB\",\"kid\":\"GIL-lQB9RX4QAlWPYz-H82cCi9RybkSo0y_BLcFY5pk\",\"kty\":\"RSA\",\"n\":\"oSZ9MHrDvW2hKJei5m4CarcQ2R3Y5wX696cq8BFPdkGfsgNpSMeD-PsOTVQTtcp143B3BGlwp7Gti21Ku7cf2rL3XhNUyCthcGow2YJ89aVffy1hfXSuhKuivcTGZTPAcbGAbW2eoOmR46MPNnLue3dZIloVtnuPGD6R2N8EKQ8hT5evkgR6T7jVmeM2Pp-TgXygg13EQdIDkzrS26-Wjq5qfAWsqfxS1_T77fC4Q1XXp4VPsM-SGUzYHY6idSfprMHK3ndOVIl8DA3_hrVfmFQ3X15Su11Tsu6jNKCm5Q74Y5laoN5gBE-_98n94DJkN9NCqAYv8QlXS_ACV_T13w\"}]}")));
+            .willReturn(okJson(jwks)));
   }
 
   private HttpEntity<Map<String, Object>> createOrderRequestEntity(HttpHeaders headersToAdd) {
